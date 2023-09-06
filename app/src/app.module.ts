d6e1a9528c455216a598f17258df7b5485ee49b4
takanori-higaki-lvgs <takanori.higaki@leverages.jp>
2023-09-06 15:52:34 +0900

    nestjsからdb接続

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'user',
+      password: 'password',
+      database: 'spreadsheet',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
