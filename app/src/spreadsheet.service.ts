b52bf5ef4527a1785c8738324c7e7b4bd1875c8e
takanori-higaki-lvgs <takanori.higaki@leverages.jp>
2023-09-08 19:55:22 +0900

    サービスアカウントによるGoogleSpreadsheetの情報取得

--- app/src/spreadsheet.service.ts
+++ app/src/spreadsheet.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { SpreadsheetEnv } from './config/spreadsheet/spreadsheet.config';
-// import { GoogleSpreadsheet } from 'google-spreadsheet';
-// import { JWT } from 'google-auth-library';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { JWT } from 'google-auth-library';
 
 @Injectable()
 export class SpreadsheetService {
@@ -12,21 +12,19 @@ export class SpreadsheetService {
   }
 
   private async fetchSpreadsheetData(): Promise<void> {
-    console.log(process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL);
     const spreadsheetEnv = new SpreadsheetEnv(new ConfigService());
-    console.log(spreadsheetEnv.googleServiceAccountEmail);
-    // const serviceAccountAuth = new JWT({
-    //   email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    //   key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    //   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-    // });
-    // const doc = new GoogleSpreadsheet(
-    //   process.env.GOOGLE_SPREADSHEET_ID,
-    //   serviceAccountAuth,
-    // );
-
-    // await doc.loadInfo();
-    // console.log(doc.title);
+    const serviceAccountAuth = new JWT({
+      keyFile: 'service_account/docker-practice-398306-548170a0c01d.json',
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+    const doc = new GoogleSpreadsheet(
+      spreadsheetEnv.googleSpreadsheetId,
+      serviceAccountAuth,
+    );
+    doc.loadInfo().then(() => {
+      console.log(doc.title);
+      console.log(doc.sheetsByIndex[0].title);
+    });
   }
 
   private saveSpreadsheetData(): void {}
