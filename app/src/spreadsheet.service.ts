383f9de8a5fd6f15db9267864612799a2de21e0d
takanori-higaki-lvgs <takanori.higaki@leverages.jp>
2023-09-12 18:29:18 +0900

    ssのデータをDBに保存するように修正。

--- app/src/spreadsheet.service.ts
+++ app/src/spreadsheet.service.ts
@@ -4,38 +4,62 @@ import { SpreadsheetEnv } from './config/spreadsheet/spreadsheet.config';
 import { GoogleSpreadsheet } from 'google-spreadsheet';
 import { JWT } from 'google-auth-library';
 import { UserService } from './user/user.service';
-import { User, Prisma } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class SpreadsheetService {
   constructor(private readonly userService: UserService) {}
 
-  async importSpreadsheet(): Promise<void> {
-    await this.fetchSpreadsheetData();
-    await this.saveSpreadsheetData();
+  async importSpreadsheet(latestUserId: number = null): Promise<void> {
+    const userList = await this.fetchSpreadsheetData(latestUserId);
+    await this.saveSpreadsheetData(userList);
   }
 
-  private async fetchSpreadsheetData(): Promise<void> {
+  private async fetchSpreadsheetData(
+    latestUserId: number = null,
+  ): Promise<Array<Prisma.UserCreateInput>> {
     const spreadsheetEnv = new SpreadsheetEnv(new ConfigService());
+
+    // GoogleServiceAccount情報
     const serviceAccountAuth = new JWT({
       keyFile: 'service_account/docker-practice-398306-548170a0c01d.json',
       scopes: ['https://www.googleapis.com/auth/spreadsheets'],
     });
+    // スプレッドシートのIDを指定して、ドキュメントオブジェクトを作成
     const doc = new GoogleSpreadsheet(
       spreadsheetEnv.googleSpreadsheetId,
       serviceAccountAuth,
     );
-    doc.loadInfo().then(() => {
+
+    const userList: Array<Prisma.UserCreateInput> = [];
+    // ドキュメントのデータを読み込む
+    await doc.loadInfo().then(async () => {
       console.log(doc.title);
       console.log(doc.sheetsByIndex[0].title);
+      const title = doc.sheetsByIndex[0].title;
+      const sheet = doc.sheetsByTitle[title];
+      const options = {
+        offset: latestUserId,
+        limit: 100,
+      };
+      const rows = await sheet.getRows(options);
+      rows.map((row) => {
+        const newUser: Prisma.UserCreateInput = {
+          name: row.get('NAME'),
+          data: row.get('DATA'),
+        };
+        userList.push(newUser);
+      });
     });
+
+    return userList;
   }
 
-  private async saveSpreadsheetData(): Promise<User> {
-    const userCreateInput: Prisma.UserCreateInput = {
-      name: 'test',
-      data: 'testdata',
-    };
-    return await this.userService.createUser(userCreateInput);
+  private async saveSpreadsheetData(
+    userList: Array<Prisma.UserCreateInput>,
+  ): Promise<void> {
+    userList.map(async (user) => {
+      await this.userService.createUser(user);
+    });
   }
 }
