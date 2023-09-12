9b0d5001ba94b6f6ad4253b1644371ab039d35ab
takanori-higaki-lvgs <takanori.higaki@leverages.jp>
2023-09-12 14:09:42 +0900

    不要なコードの削除

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SpreadsheetEnvModule } from './config/spreadsheet/spreadsheet.module';
 import { DatabaseModule } from './config/database/database.module';
 
 @Module({
   imports: [DatabaseModule, SpreadsheetEnvModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
